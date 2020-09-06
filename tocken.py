1cef25b2459dfde1fc49f3b25b2ee889978a5a79
Douglas Soch <sochdouglas1623@gmail.com>
2020-09-06 16:58:32 -0600

    Se hizo de manera correcta los identificadores de javascript

--- tocken.py
+++ tocken.py
@@ -1,30 +1,60 @@
+from enum import Enum
 
-def tipoTocken(indice):
-    respuesta = ""
-    if indice == 1:
-        respuesta = "CADENA"
-    if indice == 2:
-        respuesta = "SIMBOLO"
-    if indice == 3:
-        respuesta = "PALABRA_RESERVADA"    
-    if indice == 4:
-        respuesta = "NUMERO_ENTERO"
-    if indice == 5:
-        respuesta = "NUMERO_REAL"
-    if indice == 6:
-        respuesta = "SIGNO_MAS"
-    if indice == 7:
-        respuesta = "SIGNO_MEN"
-    if indice == 8:
-        respuesta = "SIGNO_POR"
-    if indice == 9:
-        respuesta = "SIGNO_DIV"
-    if indice == 10:
-        respuesta = "SIGNO_POW"
-    if indice == 11:
-        respuesta = "PRS_IZQ"
-    if indice == 12:
-        respuesta = "PRS_DER"
-    if  indice == 13:
-        respuesta = "CORCHETE"
-    return respuesta
+class Tipo(Enum):
+    # Simbolos del lenguaje
+    LLAVEIZQ = 1
+    LLAVEDER = 2
+    PARENT_IZQ = 3
+    PARENT_DER = 4
+    DOSPUNTOS = 5
+    PUNTOCOMA = 6
+    COMA = 7
+    PUNTO = 8
+
+    # Expresiones Logicas Relacionales
+    SIGNO_IGUAL = 9
+    SIGNO_DISTINTO= 10
+    SIGNO_MAYORQ = 11
+    SIGNO_MENORQ = 12
+    SIGNO_MAYORIGUAL = 13
+    SIGNO_MENORIGUAL = 14
+
+    # Expresiones Logicas del Lenguaje
+    CONJUNCION = 15 #&&
+    DISYUNCION = 16 #||
+    NEGACION = 17 #!EXP
+
+    # Palabras Reservadas
+    VAR = 18
+    IF = 19
+    ELSE = 20
+    ELSE_IF = 21
+    FOR = 22
+    WHILE = 23
+    DO = 24
+    CONTINUE = 25
+    BREAK = 26
+    RETURN = 27
+    FUNCTION = 28
+    CONSTRUCTOR = 29
+    CLASS = 30
+    MATH_POW = 31
+
+    # Expresion Aritmetica
+    SUMA = 32
+    RESTA = 33
+    MULTIPLICACION = 34
+    DIVISION = 35
+    POTENCIA = 36
+
+    # Expresiones Regulares
+    VALOR = 37 # Es el reconocimiento de un entero seguido de una cadena o solo un entero
+    ID = 38
+    NINGUNO = 39
+
+class Token:
+    tipoToken = Tipo.NINGUNO
+    valorToken = ""
+    def __init__(self, tipo, valor):
+        self.tipoToken = tipo
+        self.valorToken = valor
